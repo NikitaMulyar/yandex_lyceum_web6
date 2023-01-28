45319c39d7376b439a70a9aedadf35f519444da6
NikitaMulyar <ni***ol@mail.ru>
2023-01-29 00:12:56 +0300

    Bug fixed

--- 10 аптек/main.py
+++ 10 аптек/main.py
@@ -4,54 +4,57 @@ from PIL import Image
 from io import BytesIO
 
 
-search_api_server = "https://search-maps.yandex.ru/v1/"
-api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+def main():
+    search_api_server = "https://search-maps.yandex.ru/v1/"
+    api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+    address_ll = sys.argv[1]
+    search_params = {
+        "apikey": api_key,
+        "text": "аптека",
+        "lang": "ru_RU",
+        "ll": address_ll,
+        "type": "biz"
+    }
+    response = requests.get(search_api_server, params=search_params)
+    if not response:
+        print('Аптек поблизости не найдено')
+        return
 
-address_ll = sys.argv[1]
+    json_response = response.json()
+    map_params = {"ll": address_ll,
+                  "l": "map",
+                  "pt": []
+                  }
+    try:
+        if not json_response["features"]:
+            raise Exception
+        for i in json_response["features"]:
+            organization = i
+            org_hours = organization["properties"]["CompanyMetaData"]
+            if "Hours" not in org_hours:
+                org_hours = "None"
+            else:
+                org_hours = org_hours["Hours"]["text"]
+            point = organization["geometry"]["coordinates"]
+            org_point = "{0},{1}".format(point[0], point[1])
 
-search_params = {
-    "apikey": api_key,
-    "text": "аптека",
-    "lang": "ru_RU",
-    "ll": address_ll,
-    "type": "biz"
-}
+            res = ''
+            if 'круглосуточно' in org_hours:
+                res = 'pm2dgm'
+            elif org_hours == 'None':
+                res = 'pm2grm'
+            else:
+                res = 'pm2dbm'
+            map_params["pt"].append("{0},{1}".format(org_point, res))
+    except Exception:
+        print('Аптек поблизости не найдено')
+        return
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    map_params["pt"] = "~".join(map_params["pt"])
+    response = requests.get(map_api_server, params=map_params)
 
-response = requests.get(search_api_server, params=search_params)
-if not response:
-    print('error')
+    Image.open(BytesIO(
+        response.content)).show()
 
 
-json_response = response.json()
-
-map_params = {"ll": address_ll,
-              "l": "map",
-              "pt": []
-              }
-for i in json_response["features"]:
-    organization = i
-    org_hours = organization["properties"]["CompanyMetaData"]
-    if "Hours" not in org_hours:
-        org_hours = "None"
-    else:
-        org_hours = org_hours["Hours"]["text"]
-    point = organization["geometry"]["coordinates"]
-    org_point = "{0},{1}".format(point[0], point[1])
-
-    res = ''
-    if org_hours == 'круглосуточно':
-        res = 'pm2dgm'
-    elif org_hours == 'None':
-        res = 'pm2grm'
-    else:
-        res = 'pm2dbm'
-    map_params["pt"].append("{0},{1}".format(org_point, res))
-
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-map_params["pt"] = "~".join(map_params["pt"])
-print(map_params)
-response = requests.get(map_api_server, params=map_params)
-print(response.content)
-
-Image.open(BytesIO(
-    response.content)).show()
+main()
