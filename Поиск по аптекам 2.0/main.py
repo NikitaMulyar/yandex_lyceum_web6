68bb21ecb5e83669dd8c693255d505a5dcb6f035
NikitaMulyar <ni***ol@mail.ru>
2023-01-29 00:04:00 +0300

    Bug fixed

--- Поиск по аптекам 2.0/main.py
+++ Поиск по аптекам 2.0/main.py
@@ -5,56 +5,52 @@ from io import BytesIO
 from dist import lonlat_distance
 
 
-search_api_server = "https://search-maps.yandex.ru/v1/"
-api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+def main():
+    search_api_server = "https://search-maps.yandex.ru/v1/"
+    api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
-address_ll = sys.argv[1]
+    address_ll = sys.argv[1]
 
-search_params = {
-    "apikey": api_key,
-    "text": "аптека",
-    "lang": "ru_RU",
-    "ll": address_ll,
-    "type": "biz"
-}
+    search_params = {
+        "apikey": api_key,
+        "text": "аптека",
+        "lang": "ru_RU",
+        "ll": address_ll,
+        "type": "biz"
+    }
 
-response = requests.get(search_api_server, params=search_params)
-print(response.reason)
-if not response:
-    print('error')
+    response = requests.get(search_api_server, params=search_params)
+    if not response:
+        print('Аптек поблизости не найдено')
+        return
+
+    json_response = response.json()
+    try:
+        organization = json_response["features"][0]
+    except Exception:
+        print('Аптек поблизости не найдено')
+        return
+    org_name = organization["properties"]["CompanyMetaData"]["name"]
+    org_address = organization["properties"]["CompanyMetaData"]["address"]
+    point = organization["geometry"]["coordinates"]
+    org_point = "{0},{1}".format(point[0], point[1])
+
+    map_params = {
+        "ll": address_ll,
+        "l": "map",
+        "pt": "{0},pm2bm~{1},pm2am".format(org_point, address_ll)
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    Image.open(BytesIO(
+        response.content)).show()
+    print('=============================')
+    print(f'АДРЕС АПТЕКИ: {org_address}')
+    print(f'ВРЕМЯ РАБОТЫ: {organization["properties"]["CompanyMetaData"]["Hours"]["text"]}')
+    print(f'РАССТОЯНИЕ ОТ ТОЧКИ А: {lonlat_distance(tuple(map(float, address_ll.split(","))), tuple(map(float, org_point.split(",")))) / 1000}км')
+    print('=============================')
 
 
-# Преобразуем ответ в json-объект
-json_response = response.json()
-
-# Получаем первую найденную организацию.
-organization = json_response["features"][0]
-# Название организации.
-org_name = organization["properties"]["CompanyMetaData"]["name"]
-# Адрес организации.
-org_address = organization["properties"]["CompanyMetaData"]["address"]
-
-# Получаем координаты ответа.
-point = organization["geometry"]["coordinates"]
-org_point = "{0},{1}".format(point[0], point[1])
-
-# Собираем параметры для запроса к StaticMapsAPI:
-map_params = {
-    # позиционируем карту центром на наш исходный адрес
-    "ll": address_ll,
-    "l": "map",
-    # добавим точку, чтобы указать найденную аптеку
-    "pt": "{0},pm2bm~{1},pm2am".format(org_point, address_ll)
-}
-
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-# ... и выполняем запрос
-response = requests.get(map_api_server, params=map_params)
-
-Image.open(BytesIO(
-    response.content)).show()
-print('=============================')
-print(f'АДРЕС АПТЕКИ: {org_address}')
-print(f'ВРЕМЯ РАБОТЫ: {organization["properties"]["CompanyMetaData"]["Hours"]["text"]}')
-print(f'РАССТОЯНИЕ ОТ ТОЧКИ А: {lonlat_distance(tuple(map(float, address_ll.split(","))), tuple(map(float, org_point.split(",")))) / 1000}км')
-print('=============================')
+main()
